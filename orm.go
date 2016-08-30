fbcf2877c9cebc2a13972e575e107a59c3864785
qesy@163.com <qesy@163.com>
2016-08-30 16:37:36 +0800

    fix bug

--- orm.go
+++ orm.go
@@ -1,45 +1,46 @@
 package QesyDb
 
-func (m *Model)SetTable(Str string) *Model {
-    m.Table = Str
-    return m
+func (m *Model) SetTable(Str string) *Model {
+	m.Table = Str
+	return m
 }
 
-func (m *Model)SetWhere(Cond interface{}) *Model {
-    m.Cond = Cond
-    return m
+func (m *Model) SetWhere(Cond interface{}) *Model {
+	m.Cond = Cond
+	return m
 }
 
-func (m *Model)SetInsert(InsertMap map[string]string) *Model {
-    m.Insert = InsertMap
-    return m
+func (m *Model) SetInsert(InsertMap map[string]string) *Model {
+	m.Insert = InsertMap
+	return m
 }
 
-func (m *Model)SetUpdate(UpdateMap map[string]string) *Model {
-    m.Update = UpdateMap
-    return m
+func (m *Model) SetUpdate(UpdateMap map[string]string) *Model {
+	m.Update = UpdateMap
+	return m
 }
 
-func (m *Model)SetField(Field string) *Model {
-    m.Field = Field
-    return m
+func (m *Model) SetField(Field string) *Model {
+	m.Field = Field
+	return m
 }
 
-func (m *Model)SetIndex(Index string) *Model {
-    m.Index = Index
-    return m
+func (m *Model) SetIndex(Index string) *Model {
+	m.Index = Index
+	return m
 }
 
-func (m *Model)SetLimit(Limit interface{}) *Model {
-    m.Limit = Limit
-    return m
+func (m *Model) SetLimit(Limit interface{}) *Model {
+	m.Limit = Limit
+	return m
 }
 
-func (m *Model)SetSort(Sort string) *Model {
-    m.Sort = Sort
-    return m
+func (m *Model) SetSort(Sort string) *Model {
+	m.Sort = Sort
+	return m
 }
 
-func (m *Model)SetDebug(Debug int) *Model {
-    m.IsDeug = Debug
-    return m
+func (m *Model) SetDebug(Debug int) *Model {
+	m.IsDeug = Debug
+	return m
+}
