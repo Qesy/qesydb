19a812f069890792ed8b564472b060399322df8f
qesy@163.com <qesy@163.com>
2017-02-22 16:17:28 +0800

    add simple.go

---
+++ simple.go
@@ -0,0 +1,40 @@
+package QesyDb
+
+import (
+	"database/sql"
+)
+
+func ModelGet() Model {
+	var m Model
+	return m
+}
+
+func Select(TableName string, Cond map[string]string) ([]map[string]string, error) {
+	var m Model
+	return m.SetTable(TableName).SetWhere(Cond).ExecSelect()
+}
+
+func SelectOne(TableName string, Cond map[string]string) (map[string]string, error) {
+	var m Model
+	return m.SetTable(TableName).SetWhere(Cond).ExecSelectOne()
+}
+
+func Insert(TableName string, InsertArr map[string]string) (sql.Result, error) {
+	var m Model
+	return m.SetTable(TableName).SetInsert(InsertArr).ExecInsert()
+}
+
+func Replace(TableName string, InsertArr map[string]string) (sql.Result, error) {
+	var m Model
+	return m.SetTable(TableName).SetInsert(InsertArr).ExecReplace()
+}
+
+func Update(TableName string, UpdateArr map[string]string, Cond map[string]string) (sql.Result, error) {
+	var m Model
+	return m.SetTable(TableName).SetWhere(Cond).SetUpdate(UpdateArr).ExecUpdate()
+}
+
+func Delete(TableName string, Cond map[string]string) (sql.Result, error) {
+	var m Model
+	return m.SetTable(TableName).SetWhere(Cond).ExecDelete()
+}
