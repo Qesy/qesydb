26cfe80a5d1646a3feae420f4e1287e2f55e2efe
qesy@163.com <qesy@163.com>
2017-04-20 12:51:46 +0800

    add dbTx

---
+++ simpletx.go
@@ -0,0 +1,29 @@
+package QesyDb
+
+import (
+	"database/sql"
+)
+
+func SelectTx(TableName string, Cond map[string]string, m Model) ([]map[string]string, error) {
+	return m.SetTable(TableName).SetWhere(Cond).ExecSelect()
+}
+
+func SelectOneTx(TableName string, Cond map[string]string, m Model) (map[string]string, error) {
+	return m.SetTable(TableName).SetWhere(Cond).ExecSelectOne()
+}
+
+func InsertTx(TableName string, InsertArr map[string]string, m Model) (sql.Result, error) {
+	return m.SetTable(TableName).SetInsert(InsertArr).ExecInsert()
+}
+
+func ReplaceTx(TableName string, InsertArr map[string]string, m Model) (sql.Result, error) {
+	return m.SetTable(TableName).SetInsert(InsertArr).ExecReplace()
+}
+
+func UpdateTx(TableName string, UpdateArr map[string]string, Cond map[string]string, m Model) (sql.Result, error) {
+	return m.SetTable(TableName).SetWhere(Cond).SetUpdate(UpdateArr).ExecUpdate()
+}
+
+func DeleteTx(TableName string, Cond map[string]string, m Model) (sql.Result, error) {
+	return m.SetTable(TableName).SetWhere(Cond).ExecDelete()
+}
